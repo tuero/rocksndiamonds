9259f166dce493dc4114eb6e5fff7bc52a281c7b
Holger Schemel <info@artsoft.org>
2006-07-16 14:47:47 +0200

    rnd-20060716-1-src * version number set to 3.2.1 * version 3.2.0 released

--- src/main.h
+++ src/main.h
@@ -1714,8 +1714,8 @@
 /* program information and versioning definitions */
 #define PROGRAM_VERSION_MAJOR		3
 #define PROGRAM_VERSION_MINOR		2
-#define PROGRAM_VERSION_PATCH		0
-#define PROGRAM_VERSION_BUILD		9
+#define PROGRAM_VERSION_PATCH		1
+#define PROGRAM_VERSION_BUILD		0
 
 #define PROGRAM_TITLE_STRING		"Rocks'n'Diamonds"
 #define PROGRAM_AUTHOR_STRING		"Holger Schemel"
