c4ebc9acc10cfdcdfb88a3aa8d7a32cdd6cf8b6e
Holger Schemel <info@artsoft.org>
2016-11-29 20:02:06 +0100

    added linking to GLESv2 library

--- src/Android.mk
+++ src/Android.mk
@@ -102,6 +102,6 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
 
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_net smpeg2
 
-LOCAL_LDLIBS := -lGLESv1_CM -llog
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
 include $(BUILD_SHARED_LIBRARY)
