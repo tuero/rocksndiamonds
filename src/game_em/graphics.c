tuero/rocksndiamonds

commit 727d3b5fc7cac6f59652eaa23f1ed9e991ae50bd

added some performance tuning for "autotest" and warp mode
- profiling showed that completely skipping the playfield redraw functions
  makes "autotest" and warp mode much (or at least considerably) faster in
  certain cases than only skipping the resulting bitmap blitting functions
- massive speedup when using the EM engine with the R'n'D graphics engine
- good speedup when using the EM engine with the native EM graphics engine
- good speedup when using the SP engine
- minor speedup when using the R'n'D engine

src/game_em/graphics.c
@@ -544,6 +544,10 @@ static boolean checkIfAllPlayersAreVisible(int center_x, int center_y)
 
 void RedrawPlayfield_EM(boolean force_redraw)
 {
+  // skip redrawing playfield in warp mode or when testing tapes with "autotest"
+  if (DrawingDeactivatedField())
+    return;
+
   boolean draw_new_player_location = FALSE;
   boolean quick_relocation = setup.quick_switch;
   int max_center_distance_player_nr =

src/game.c
@@ -4599,6 +4599,10 @@ void InitPlayerGfxAnimation(struct PlayerInfo *player, int action, int dir)
 
 static void ResetGfxFrame(int x, int y)
 {
+  // profiling showed that "autotest" spends 10~20% of its time in this function
+  if (DrawingDeactivatedField())
+    return;
+
   int element = Feld[x][y];
   int graphic = el_act_dir2img(element, GfxAction[x][y], GfxDir[x][y]);
 

src/game_sp/main.c
@@ -54,6 +54,10 @@ void InitGameEngine_SP()
 
 void RedrawPlayfield_SP(boolean force_redraw)
 {
+  // skip redrawing playfield in warp mode or when testing tapes with "autotest"
+  if (DrawingDeactivatedField())
+    return;
+
   if (force_redraw)
     RestorePlayfield();
 

src/libgame/system.c
@@ -600,6 +600,17 @@ inline static boolean CheckDrawingArea(int x, int y, int width, int height,
   return FALSE;
 }
 
+boolean DrawingDeactivatedField()
+{
+  if (program.headless)
+    return TRUE;
+
+  if (gfx.draw_deactivation_mask & REDRAW_FIELD)
+    return TRUE;
+
+  return FALSE;
+}
+
 boolean DrawingDeactivated(int x, int y, int width, int height)
 {
   return CheckDrawingArea(x, y, width, height, gfx.draw_deactivation_mask);

src/libgame/system.h
@@ -1505,6 +1505,7 @@ void FillRectangle(Bitmap *, int, int, int, int, Pixel);
 void ClearRectangle(Bitmap *, int, int, int, int);
 void ClearRectangleOnBackground(Bitmap *, int, int, int, int);
 void BlitBitmapMasked(Bitmap *, Bitmap *, int, int, int, int, int, int);
+boolean DrawingDeactivatedField(void);
 boolean DrawingDeactivated(int, int, int, int);
 boolean DrawingOnBackground(int, int);
 boolean DrawingAreaChanged();

commit 1634a3a8e6725c9d3a31c07acafb8b8a8946f881

fixed bug with not updating screen scroll position in EM engine in warp mode
- this bug was introduced with commit 727d3b5f

src/game_em/graphics.c
@@ -544,10 +544,6 @@ static boolean checkIfAllPlayersAreVisible(int center_x, int center_y)
 
 void RedrawPlayfield_EM(boolean force_redraw)
 {
-  // skip redrawing playfield in warp mode or when testing tapes with "autotest"
-  if (DrawingDeactivatedField())
-    return;
-
   boolean draw_new_player_location = FALSE;
   boolean quick_relocation = setup.quick_switch;
   int max_center_distance_player_nr =
@@ -763,6 +759,10 @@ void RedrawPlayfield_EM(boolean force_redraw)
 	(dy > 0 && player_move_dir != MV_DOWN))
       screen_y = screen_y_old;
   }
+
+  // skip redrawing playfield in warp mode or when testing tapes with "autotest"
+  if (DrawingDeactivatedField())
+    return;
 
   animscreen();
 