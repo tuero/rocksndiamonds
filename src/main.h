7b1ffea2177d102ab48ecde6c9fdfdb1b0dae0d8
Holger Schemel <info@artsoft.org>
2018-04-06 15:18:39 +0200

    version number set to 4.1.0.0

--- src/main.h
+++ src/main.h
@@ -2514,9 +2514,9 @@
 
 /* program information and versioning definitions */
 #define PROGRAM_VERSION_MAJOR		4
-#define PROGRAM_VERSION_MINOR		0
-#define PROGRAM_VERSION_PATCH		1
-#define PROGRAM_VERSION_BUILD		5
+#define PROGRAM_VERSION_MINOR		1
+#define PROGRAM_VERSION_PATCH		0
+#define PROGRAM_VERSION_BUILD		0
 #define PROGRAM_VERSION_EXTRA		""
 
 #define PROGRAM_TITLE_STRING		"Rocks'n'Diamonds"
