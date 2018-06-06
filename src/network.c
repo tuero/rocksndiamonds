77ece4b955d08d19d0f05bf2d8241467a06d4bab
Holger Schemel <info@artsoft.org>
2018-06-06 08:43:28 +0200

    fixed showing message request if local player stopped network game

--- src/network.c
+++ src/network.c
@@ -544,13 +544,17 @@ static void Handle_OP_STOP_PLAYING()
 
   if (game_status == GAME_MODE_PLAYING)
   {
+    int client_nr = buffer[0];
+    int index_nr = client_nr - 1;
+    boolean stopped_by_remote_player = (index_nr != local_player->index_nr);
     char *message = (buffer[2] == NETWORK_STOP_BY_PLAYER ?
 		     "Network game stopped by player!" :
 		     buffer[2] == NETWORK_STOP_BY_ERROR ?
 		     "Network game stopped due to internal error!" :
 		     "Network game stopped!");
 
-    Request(message, REQ_CONFIRM | REQ_STAY_CLOSED);
+    if (buffer[2] != NETWORK_STOP_BY_PLAYER || stopped_by_remote_player)
+      Request(message, REQ_CONFIRM | REQ_STAY_CLOSED);
   }
 
   SetGameStatus(GAME_MODE_MAIN);
