tuero/rocksndiamonds

commit 931d95b5a399bbed7280bd39be8373adad147dc2

fixed bug with player moving when releasing one of several pressed snap keys
When using TAS snap keys, pressing more than one snap key at the same
time and then releasing one of them caused the player to move.

src/events.c
@@ -2015,6 +2015,7 @@ void HandleKey(Key key, int key_status)
     for (pnr = 0; pnr < MAX_PLAYERS; pnr++)
     {
       byte key_action = 0;
+      byte key_snap_action = 0;
 
       if (setup.input[pnr].use_joystick)
 	continue;
@@ -2030,15 +2031,33 @@ void HandleKey(Key key, int key_status)
       {
 	ssi = setup.shortcut;
 
+	// also remember normal snap key when handling snap+direction keys
+	key_snap_action |= key_action & JOY_BUTTON_SNAP;
+
 	for (i = 0; i < NUM_DIRECTIONS; i++)
+	{
 	  if (key == *key_info[i].key_snap)
-	    key_action |= key_info[i].action | JOY_BUTTON_SNAP;
+	  {
+	    key_action      |= key_info[i].action | JOY_BUTTON_SNAP;
+	    key_snap_action |= key_info[i].action;
+	  }
+	}
       }
 
       if (key_status == KEY_PRESSED)
-	stored_player[pnr].action |= key_action;
+      {
+	stored_player[pnr].action      |= key_action;
+	stored_player[pnr].snap_action |= key_snap_action;
+      }
       else
-	stored_player[pnr].action &= ~key_action;
+      {
+	stored_player[pnr].action      &= ~key_action;
+	stored_player[pnr].snap_action &= ~key_snap_action;
+      }
+
+      // restore snap action if one of several pressed snap keys was released
+      if (stored_player[pnr].snap_action)
+	stored_player[pnr].action |= JOY_BUTTON_SNAP;
 
       if (tape.single_step && tape.recording && tape.pausing && !tape.use_mouse)
       {

src/game.c
@@ -3402,6 +3402,7 @@ void InitGame(void)
     player->action = 0;
     player->effective_action = 0;
     player->programmed_action = 0;
+    player->snap_action = 0;
 
     player->mouse_action.lx = 0;
     player->mouse_action.ly = 0;

src/game.h
@@ -281,6 +281,7 @@ struct PlayerInfo
 				   devices when in single player mode */
   byte programmed_action;	/* action forced by game itself (like moving
 				   through doors); overrides other actions */
+  byte snap_action;		// action from TAS snap keys
 
   struct MouseActionInfo mouse_action;		 // (used by MM engine only)
   struct MouseActionInfo effective_mouse_action; // (used by MM engine only)

commit 9db7595a83420f029dd52a462d766d4ac585e9c7

fixed bug when snapping envelope (introduced by commit 931d95b5)

src/events.c
@@ -405,7 +405,10 @@ void ClearPlayerAction(void)
   // simulate key release events for still pressed keys
   key_joystick_mapping = 0;
   for (i = 0; i < MAX_PLAYERS; i++)
+  {
     stored_player[i].action = 0;
+    stored_player[i].snap_action = 0;
+  }
 
   ClearJoystickState();
   ClearPlayerMouseAction();