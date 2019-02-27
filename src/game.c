tuero/rocksndiamonds

commit f77bbb8fc772782a6d4169d22a45716e9d4d0813

fixed bug with different global border when playing from level editor
When using different screen border images for the in-game screen
(while playing the game, as defined by "global.border.PLAYING") and
for the level editor (defined by "global.border.EDITOR"), test-playing
a level from the level editor resulted in a graphically broken global
screen border (while everything looks correct when playing from the
main menu).

src/game.c
@@ -3337,8 +3337,7 @@ void InitGame()
 
   ExpireSoundLoops(TRUE);
 
-  if (!level_editor_test_game)
-    FadeOut(fade_mask);
+  FadeOut(fade_mask);
 
   /* needed if different viewport properties defined for playing */
   ChangeViewportPropertiesIfNeeded();

commit 3ea7bc006da99f1b13d817285820023b30ce2e38

fixed graphical bugs caused by commit f77bbb8f and 1b4dc759
Each of these two commits exactly do the reverse of the other commit,
both still resulting in graphical bugs (that the corresponding other
commit fixes).

This change should fix both problems, resulting in (a) playfield not
flashing black when game started from level editor and (b) different
global borders when playing from level editor being correctly drawn.

src/game.c
@@ -3354,7 +3354,7 @@ void InitGame(void)
   SetGameStatus(GAME_MODE_PLAYING);
 
   if (level_editor_test_game)
-    FadeSkipNextFadeIn();
+    FadeSkipNextFadeOut();
   else
     FadeSetEnterScreen();
 
@@ -3365,8 +3365,10 @@ void InitGame(void)
 
   ExpireSoundLoops(TRUE);
 
-  if (!level_editor_test_game)
-    FadeOut(fade_mask);
+  FadeOut(fade_mask);
+
+  if (level_editor_test_game)
+    FadeSkipNextFadeIn();
 
   // needed if different viewport properties defined for playing
   ChangeViewportPropertiesIfNeeded();

src/tools.c
@@ -1064,7 +1064,8 @@ void FadeIn(int fade_mask)
 void FadeOut(int fade_mask)
 {
   // update screen if areas covered by "fade_mask" and "redraw_mask" differ
-  if (!equalRedrawMasks(fade_mask, redraw_mask))
+  if (!equalRedrawMasks(fade_mask, redraw_mask) &&
+      fade_type_skip != FADE_MODE_SKIP_FADE_OUT)
     BackToFront();
 
   SetScreenStates_BeforeFadingOut();