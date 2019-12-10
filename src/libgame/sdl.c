tuero/rocksndiamonds

commit a2698b3b66ff48d2bccb5015384e09d8110df6eb

changed error handling and messages when loading images

src/libgame/sdl.c
@@ -2188,7 +2188,7 @@ static SDL_Surface *SDLGetOpaqueSurface(SDL_Surface *surface)
     return NULL;
 
   if ((new_surface = SDLGetNativeSurface(surface)) == NULL)
-    Error(ERR_EXIT, "SDL_DisplayFormat() failed");
+    Error(ERR_EXIT, "SDLGetNativeSurface() failed");
 
   /* remove alpha channel from native non-transparent surface, if defined */
   SDLSetAlpha(new_surface, FALSE, 0);
@@ -2247,11 +2247,7 @@ Bitmap *SDLLoadImage(char *filename)
 
   /* load image to temporary surface */
   if ((sdl_image_tmp = IMG_Load(filename)) == NULL)
-  {
-    SetError("IMG_Load(): %s", SDL_GetError());
-
-    return NULL;
-  }
+    Error(ERR_EXIT, "IMG_Load() failed: %s", SDL_GetError());
 
   print_timestamp_time("IMG_Load");
 
@@ -2259,11 +2255,7 @@ Bitmap *SDLLoadImage(char *filename)
 
   /* create native non-transparent surface for current image */
   if ((new_bitmap->surface = SDLGetNativeSurface(sdl_image_tmp)) == NULL)
-  {
-    SetError("SDL_DisplayFormat(): %s", SDL_GetError());
-
-    return NULL;
-  }
+    Error(ERR_EXIT, "SDLGetNativeSurface() failed");
 
   /* remove alpha channel from native non-transparent surface, if defined */
   SDLSetAlpha(new_bitmap->surface, FALSE, 0);
@@ -2271,7 +2263,7 @@ Bitmap *SDLLoadImage(char *filename)
   /* remove transparent color from native non-transparent surface, if defined */
   SDL_SetColorKey(new_bitmap->surface, UNSET_TRANSPARENT_PIXEL, 0);
 
-  print_timestamp_time("SDL_DisplayFormat (opaque)");
+  print_timestamp_time("SDLGetNativeSurface (opaque)");
 
   UPDATE_BUSY_STATE();
 
@@ -2283,13 +2275,9 @@ Bitmap *SDLLoadImage(char *filename)
 
   /* create native transparent surface for current image */
   if ((new_bitmap->surface_masked = SDLGetNativeSurface(sdl_image_tmp)) == NULL)
-  {
-    SetError("SDL_DisplayFormat(): %s", SDL_GetError());
+    Error(ERR_EXIT, "SDLGetNativeSurface() failed");
 
-    return NULL;
-  }
-
-  print_timestamp_time("SDL_DisplayFormat (masked)");
+  print_timestamp_time("SDLGetNativeSurface (masked)");
 
   UPDATE_BUSY_STATE();
 

commit 368d771258ee3aed9c0713a0998094651eca2504

improved error messages when IMG_Load() fails loading an image
If loading an image using "IMG_Load()" fails, the name of the file is
now also displayed with the error message.

This already worked in the past, but was broken since commit a2698b3b.

src/libgame/sdl.c
@@ -273,7 +273,7 @@ static void SDLSetWindowIcon(char *basename)
 
   if ((surface = IMG_Load(filename)) == NULL)
   {
-    Error(ERR_WARN, "IMG_Load() failed: %s", SDL_GetError());
+    Error(ERR_WARN, "IMG_Load('%s') failed: %s", basename, SDL_GetError());
 
     return;
   }
@@ -2239,7 +2239,8 @@ Bitmap *SDLLoadImage(char *filename)
 
   // load image to temporary surface
   if ((sdl_image_tmp = IMG_Load(filename)) == NULL)
-    Error(ERR_EXIT, "IMG_Load() failed: %s", SDL_GetError());
+    Error(ERR_EXIT, "IMG_Load('%s') failed: %s", getBaseNamePtr(filename),
+	  SDL_GetError());
 
   print_timestamp_time("IMG_Load");
 