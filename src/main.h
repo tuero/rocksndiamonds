617fc4a5748a1f2f48614b5dd30915852bf39fc2
Holger Schemel <info@artsoft.org>
2020-01-03 16:51:09 +0100

    version number set to 4.1.4.0

--- src/main.h
+++ src/main.h
@@ -2548,8 +2548,8 @@ enum
 // program information and versioning definitions
 #define PROGRAM_VERSION_SUPER		4
 #define PROGRAM_VERSION_MAJOR		1
-#define PROGRAM_VERSION_MINOR		3
-#define PROGRAM_VERSION_PATCH		1
+#define PROGRAM_VERSION_MINOR		4
+#define PROGRAM_VERSION_PATCH		0
 #define PROGRAM_VERSION_EXTRA		""
 
 #define PROGRAM_TITLE_STRING		"Rocks'n'Diamonds"
